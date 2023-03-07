f750f75448955cead5336d391cb3b3e39aa7b8b1
Lisandra Cruz <lisansouza@gmail.com>
2023-03-07 20:50:47 -0300

    implementação da soma da calculadora

--- js/calculadora.js
+++ js/calculadora.js
@@ -3,23 +3,17 @@ function somar() {
   const numero2 = parseFloat(document.getElementById("numero2Soma").value);
   // evite mexer no código acima!
 
-
-
-
+  const resultado = numero1 + numero2;
 
   /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
   Em seguida, ao invés de mostrar o numero 10, mostre o resultado da operação */
-  document.getElementById("resultadoSoma").innerHTML = 10;
+  document.getElementById("resultadoSoma").innerHTML = resultado;
 }
 
 function subtrair() {
   const numero1 = parseFloat(document.getElementById("numero1Sub").value);
   const numero2 = parseFloat(document.getElementById("numero2Sub").value);
   // evite mexer no código acima!
-
-
-
-
 
   /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
    Em seguida, ao invés de mostrar o numero 9, mostre o resultado da operação */
@@ -31,10 +25,6 @@ function multiplicar() {
   const numero2 = parseFloat(document.getElementById("numero2Mult").value);
   // evite mexer no código acima!
 
-
-
-
-
   /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
    Em seguida, ao invés de mostrar o numero 5, mostre o resultado da operação */
   document.getElementById("resultadoMult").innerHTML = 5;
@@ -45,10 +35,6 @@ function dividir() {
   const numero2 = parseFloat(document.getElementById("numero2Div").value);
   // evite mexer no código acima!
 
-
-
-
-  
   /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
    Em seguida, ao invés de mostrar o numero -89, mostre o resultado da operação */
   document.getElementById("resultadoDiv").innerHTML = -89;
